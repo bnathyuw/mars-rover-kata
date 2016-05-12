a70532c4f8bda4241c7a9fd430a9dbfabcdddc8d
Matthew Butt <matthew@codurance.com>
2016-05-12 15:07:32 +0100

    Finally, we add a spoonful of sugar, and we're done.

--- C#/MarsRoverKata/MarsRoverKata/Src/Direction.cs
+++ C#/MarsRoverKata/MarsRoverKata/Src/Direction.cs
@@ -1,13 +1,8 @@
-using System;
-
 namespace MarsRoverKata.Src
 {
     abstract class Direction
     {
-        public static Direction Initial()
-        {
-            return new North();
-        }
+        public static Direction Initial() => new North();
 
         private Direction()
         {
@@ -19,74 +14,38 @@ namespace MarsRoverKata.Src
 
         private class North : Direction
         {
-            public override Direction RotateLeft()
-            {
-                return new West();
-            }
+            public override Direction RotateLeft() => new West();
 
-            public override Direction RotateRight()
-            {
-                return new East();
-            }
+            public override Direction RotateRight() => new East();
 
-            public override string ToString()
-            {
-                return "N";
-            }
+            public override string ToString() => "N";
         }
 
         private class East : Direction
         {
-            public override Direction RotateLeft()
-            {
-                return new North();
-            }
+            public override Direction RotateLeft() => new North();
 
-            public override Direction RotateRight()
-            {
-                return new South();
-            }
+            public override Direction RotateRight() => new South();
 
-            public override string ToString()
-            {
-                return "E";
-            }
+            public override string ToString() => "E";
         }
 
         private class South : Direction
         {
-            public override Direction RotateLeft()
-            {
-                return new East();
-            }
+            public override Direction RotateLeft() => new East();
 
-            public override Direction RotateRight()
-            {
-                return new West();
-            }
+            public override Direction RotateRight() => new West();
 
-            public override string ToString()
-            {
-                return "S";
-            }
+            public override string ToString() => "S";
         }
 
         private class West : Direction
         {
-            public override Direction RotateLeft()
-            {
-                return new South();
-            }
+            public override Direction RotateLeft() => new South();
 
-            public override Direction RotateRight()
-            {
-                return new North();
-            }
+            public override Direction RotateRight() => new North();
 
-            public override string ToString()
-            {
-                return "W";
-            }
+            public override string ToString() => "W";
         }
     }
 }
